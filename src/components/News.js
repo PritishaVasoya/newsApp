1eaa7a6ab24fe6d71e657e2af2e24411e1d8c5d6
PritishaVasoya <pritishavasoya777@gmail.com>
2026-03-12 18:20:57 +0530

    Add some changes

--- src/components/News.js
+++ src/components/News.js
@@ -46,7 +46,7 @@ export class News extends Component {
 
                     {this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
 
